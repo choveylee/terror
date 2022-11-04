6a2abb30e92ec8fa261af0d8dacaae8b9593fbfe
lidonglin@rcrai.com <4388193@qq.com>
2022-11-04 11:29:24 +0800

    update stack track

--- terror.go
+++ terror.go
@@ -10,34 +10,45 @@ package terror
 
 import (
 	"context"
+	"fmt"
 	"net/http"
-
-	"github.com/getsentry/raven-go"
+	"runtime"
+	"strings"
 )
 
 type Terror struct {
 	err        error
 	errCode    int
 	errMsg     []string
-	stackTrack *raven.Stacktrace
+	stackTrack string
 	request    *http.Request
 }
 
 func NewTerror(ctx context.Context, err error, code int, msg string) *Terror {
-	return &Terror{
-		err:        err,
-		errCode:    code,
-		errMsg:     []string{msg},
-		stackTrack: raven.NewStacktrace(1, 3, []string{}),
+	terror := &Terror{
+		err:     err,
+		errCode: code,
+		errMsg:  []string{msg},
 	}
+
+	_, file, line := funcFileLine("github.com/choveylee")
+
+	terror.stackTrack = fmt.Sprintf("%s:%d", file, line)
+
+	return terror
 }
 
 func NewRawTerror(ctx context.Context, err error, msg string) *Terror {
-	return &Terror{
-		err:        err,
-		errMsg:     []string{msg},
-		stackTrack: raven.NewStacktrace(1, 3, []string{}),
+	terror := &Terror{
+		err:    err,
+		errMsg: []string{msg},
 	}
+
+	_, file, line := funcFileLine("github.com/choveylee")
+
+	terror.stackTrack = fmt.Sprintf("%s:%d", file, line)
+
+	return terror
 }
 
 func (p *Terror) AttachRequest(request *http.Request) *Terror {
@@ -62,4 +73,31 @@ func (p *Terror) AttachErrMsg(msg string) *Terror {
 	p.errMsg = append(p.errMsg, msg)
 
 	return p
+}
+
+func funcFileLine(excludePKG string) (string, string, int) {
+	const depth = 8
+	var pcs [depth]uintptr
+	n := runtime.Callers(3, pcs[:])
+	ff := runtime.CallersFrames(pcs[:n])
+
+	var fn, file string
+	var line int
+	for {
+		f, ok := ff.Next()
+		if !ok {
+			break
+		}
+
+		fn, file, line = f.Function, f.File, f.Line
+		if !strings.Contains(fn, excludePKG) {
+			break
+		}
+	}
+
+	if index := strings.LastIndexByte(fn, '/'); index != -1 {
+		fn = fn[index+1:]
+	}
+
+	return fn, file, line
 }
